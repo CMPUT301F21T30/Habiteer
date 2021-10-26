689fc25147b2880e8874af5bcf55a249cb86aa2f
Geoff Banh <38046772+geoff-B@users.noreply.github.com>
2021-10-26 16:07:35 -0600

    fix import

--- app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/addEditHabit/AddEditHabitFragment.java
+++ app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/addEditHabit/AddEditHabitFragment.java
@@ -12,7 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.CMPUT301F21T30.Habiteer.ui.addEditHabit.R;
+import com.CMPUT301F21T30.Habiteer.R;
 
 public class AddEditHabitFragment extends Fragment {
 
