99e8146c47058edb4191263be364ad26c6411543
Kavit Gami <kavit.gami@gmail.com>
2021-11-02 22:53:46 -0600

    Updated Delete Habit
    Work in progress

--- app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/habit/DeleteHabit.java
+++ app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/habit/DeleteHabit.java
@@ -4,11 +4,16 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.CMPUT301F21T30.Habiteer.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -36,9 +41,28 @@ public class DeleteHabit extends AppCompatActivity {
                 dialog.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
+                        firebaseUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(DeleteHabit.this, "Habit Deleted",Toast.LENGTH_LONG).show();
+                                }else{
+                                    Toast.makeText(DeleteHabit.this, task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
                     }
-                })
+                });
+
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                       dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+
 
             }
         });
