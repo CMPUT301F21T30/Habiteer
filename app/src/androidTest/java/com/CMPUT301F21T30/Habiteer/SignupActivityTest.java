8339fe61a2928c3f1a1ad1928191999a239187c2
Harnoor Josan <harnoorvirjosan@gmail.com>
2021-11-05 14:48:46 -0600

    Testing for signup completed

--- app/src/androidTest/java/com/CMPUT301F21T30/Habiteer/SignupActivityTest.java
+++ app/src/androidTest/java/com/CMPUT301F21T30/Habiteer/SignupActivityTest.java
@@ -1,10 +1,16 @@
 package com.CMPUT301F21T30.Habiteer;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import android.app.Activity;
+import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import com.robotium.solo.Solo;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -38,28 +44,89 @@ public class SignupActivityTest {
     }
 
     /**
-     * Add an emaail and a password and check the city name using assertTrue
-     * Clear all the cities from the listview and check again with assertFalse
+     * Add an email, a password and a confirm password and check it using assertTrue
+     * Clear the everything and check again with assertFalse
      */
-    //@Test
-    //public void checkList(){
-// Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
-        //solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        //solo.clickOnButton("ADD CITY"); //Click ADD CITY Button
-//Get view for EditText and enter a city name
-        //solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
-        //solo.clickOnButton("CONFIRM"); //Select CONFIRM Button
-        //solo.clearEditText((EditText) solo.getView(R.id.editText_name)); //Clear the EditText
-/* True if there is a text: Edmonton on the screen, wait at least 2 seconds and find
-minimum one match. */
-        //assertTrue(solo.waitForText("Edmonton", 1, 2000));
-        //solo.clickOnButton("ClEAR ALL"); //Select ClEAR ALL
-//True if there is no text: Edmonton on the screen
-        //assertFalse(solo.searchText("Edmonton"));
+    @Test
+    public void checkEmailPassword(){
+        // Asserts that the current activity is the SignupActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", SignupActivity.class);
+
+        //Get view for EditText and enter an Email, Password and confirm Password
+        solo.enterText((EditText) solo.getView(R.id.signupEmail), "123@gm.ca");
+        solo.enterText((EditText) solo.getView(R.id.signupPassword), "123456");
+        solo.enterText((EditText) solo.getView(R.id.signupConfirmPassword), "123456");
+
+
+
+        /* True if texts are there on screen on the screen, wait at least 2 seconds and find minimum one match for email and 2 for passwords. */
+        assertTrue(solo.waitForText("123@gm.ca", 1, 2000));
+        assertTrue(solo.waitForText("123456", 2, 2000));
+
+        //Clear the EditText
+        solo.clearEditText((EditText) solo.getView(R.id.signupEmail));
+        solo.clearEditText((EditText) solo.getView(R.id.signupPassword));
+        solo.clearEditText((EditText) solo.getView(R.id.signupConfirmPassword));
+
+        //True if there is no text on the screen
+        assertFalse(solo.searchText("123@gm.ca"));
+        assertFalse(solo.searchText("123456"));
     }
 
+    /**
+     * Press the Login Button and go to LoginActivity
+     */
+    @Test
+    public void checkLoginBtn(){
+        // Asserts that the current activity is the SignupActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", SignupActivity.class);
 
+        //Click on the Login Button
+        solo.clickOnButton("Login"); //Select Login Button
+
+        // Asserts that the current activity is the LoginActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+
+    }
+
+    /**
+     * Press the Signup Button and go to MainActivity
+     */
+    @Test
+    public void checkSignupBtn(){
+        // Asserts that the current activity is the SignupActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", SignupActivity.class);
+
+        //Get view for EditText and enter an Email, Password and confirm Password
+        solo.enterText((EditText) solo.getView(R.id.signupEmail), "123@gm.ca");
+        solo.enterText((EditText) solo.getView(R.id.signupPassword), "123456");
+        solo.enterText((EditText) solo.getView(R.id.signupConfirmPassword), "123456");
+
+        //Click on the Login Button
+        solo.clickOnButton("Sign up"); //Select Sign up Button
+
+        // Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+    }
+
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
 }
 
 
-}
+
+
+
+
+
+
+
+
+
+
