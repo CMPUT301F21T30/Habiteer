a56264585a8d008d72d4739575b36bce88efee46
Geoff Banh <38046772+geoff-B@users.noreply.github.com>
2021-11-05 13:29:25 -0600

    separate addHabit tests into own file

---
+++ app/src/androidTest/java/com/CMPUT301F21T30/Habiteer/AddHabitTests.java
@@ -0,0 +1,52 @@
+package com.CMPUT301F21T30.Habiteer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.widget.EditText;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.CMPUT301F21T30.Habiteer.ui.addEditHabit.AddEditHabitActivity;
+import com.CMPUT301F21T30.Habiteer.ui.addEditHabit.AddEditHabitFragment;
+import com.google.android.material.textfield.TextInputLayout;
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class AddHabitTests {
+    private Solo solo;
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.CMPUT301F21T30.Habiteer", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<SignupActivity> rule = new ActivityTestRule<>(SignupActivity.class, true, true);
+
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        solo.clickOnView(solo.getView(R.id.goToLoginBtn));
+        solo.enterText((EditText) solo.getView(R.id.loginEmail), "as@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.loginPassword), "123456");
+        solo.clickOnView(solo.getView(R.id.loginBtn));
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+    }
+    
+}
