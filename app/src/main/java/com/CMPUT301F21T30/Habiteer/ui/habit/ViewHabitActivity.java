4975a692ad089513785c48feb8515f2c4567346f
Shyam Varsani <shyammulji@outlook.com>
2021-11-05 01:29:10 -0600

    Added intent to events

--- app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/habit/ViewHabitActivity.java
+++ app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/habit/ViewHabitActivity.java
@@ -15,10 +15,13 @@ import com.CMPUT301F21T30.Habiteer.MainActivity;
 import com.CMPUT301F21T30.Habiteer.R;
 import com.CMPUT301F21T30.Habiteer.Session;
 import com.CMPUT301F21T30.Habiteer.ui.addEditHabit.AddEditHabitActivity;
+import com.CMPUT301F21T30.Habiteer.ui.habitEvents.AddHabitEventActivity;
 import com.google.android.gms.common.util.ArrayUtils;
 import com.google.common.collect.Lists;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +33,9 @@ public class ViewHabitActivity extends AppCompatActivity {
     ProgressBar progress;
     Switch privateSwitch;
     MaterialDayPicker days;
+    Calendar calendar;
+    String todayDate;
+    SimpleDateFormat dateFormat;
 
 
     @Override
@@ -52,8 +58,6 @@ public class ViewHabitActivity extends AppCompatActivity {
         progress = findViewById(R.id.progress);
         privateSwitch = findViewById(R.id.privateSwitch);
 
-
-
         // get the habit index from the intent
         Bundle bundle = getIntent().getExtras();
         int habitIndex = bundle.getInt("habitIndex");
@@ -62,8 +66,6 @@ public class ViewHabitActivity extends AppCompatActivity {
 
         // get current habit at that index
         Habit currentHabit = Session.getInstance().getUser().getHabitList().get(habitIndex);
-
-
 
         // get habit info
         String habitname = currentHabit.getHabitName();
@@ -107,6 +109,13 @@ public class ViewHabitActivity extends AppCompatActivity {
         addHabitEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                todayDate = dateFormat.format(calendar.getTime());
+                Intent intent = new Intent(ViewHabitActivity.this, AddHabitEventActivity.class);
+                intent.putExtra("habitIndex", habitIndex);
+                intent.putExtra("eventDate", todayDate);
+                startActivity(intent);
 //                startActivity(new Intent(getApplicationContext(), AddHabitEvent.class)); //the user goes to the addHabitEvent activity
 
             }
