5682749e500e7f7bc77be1d2a2ed4af339d3d5a2
Shyam Varsani <shyammulji@outlook.com>
2021-11-05 15:34:57 -0600

    The test fully works now after changing account used for login

--- app/src/androidTest/java/com/CMPUT301F21T30/Habiteer/AddHabitEventTest.java
+++ app/src/androidTest/java/com/CMPUT301F21T30/Habiteer/AddHabitEventTest.java
@@ -33,7 +33,7 @@ public class AddHabitEventTest {
     public void setUp() throws Exception {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
         solo.clickOnView(solo.getView(R.id.goToLoginBtn));
-        solo.enterText((EditText) solo.getView(R.id.loginEmail), "abc@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.loginEmail), "s@g.com");
         solo.enterText((EditText) solo.getView(R.id.loginPassword), "123456");
         solo.clickOnView(solo.getView(R.id.loginBtn));
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
