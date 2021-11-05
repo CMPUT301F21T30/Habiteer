64452f6f62b8e5c2a3b5da7c9d56c92dbfa12ded
Warren <85139891+wrrnlim@users.noreply.github.com>
2021-11-05 14:43:12 -0600

    Rename test file

--- app/src/androidTest/java/com/CMPUT301F21T30/Habiteer/ListHabitTest.java
+++ app/src/androidTest/java/com/CMPUT301F21T30/Habiteer/ListHabitTest.java
@@ -35,7 +35,7 @@ import org.junit.runner.RunWith;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class HabiteerTest {
+public class ListHabitTest {
     private Solo solo;
     @Test
     public void useAppContext() {
