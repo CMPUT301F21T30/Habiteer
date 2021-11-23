1034519098cc8e4f246ea0374dc45b2362cb7940
harnoorjosan <91575135+harnoorjosan@users.noreply.github.com>
2021-11-23 15:56:34 -0700

    SearchUserFragment interface

---
+++ app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/Follow/SearchUserFragment.java
@@ -0,0 +1,38 @@
+package com.CMPUT301F21T30.Habiteer.ui.Follow;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.CMPUT301F21T30.Habiteer.R;
+
+public class SearchUserFragment extends Fragment {
+
+    private SearchUserViewModel mViewModel;
+
+    public static SearchUserFragment newInstance() {
+        return new SearchUserFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.search_user_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SearchUserViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/Follow/SearchUserViewModel.java
@@ -0,0 +1,7 @@
+package com.CMPUT301F21T30.Habiteer.ui.Follow;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchUserViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
