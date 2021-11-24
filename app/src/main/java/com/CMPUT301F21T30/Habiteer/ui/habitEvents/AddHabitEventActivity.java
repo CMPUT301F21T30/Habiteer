2bcb47bf7b27815cb487e346299a62062e651df8
Bushratun Nusaibah <91575185+Bushratun-Nusaibah@users.noreply.github.com>
2021-11-24 14:34:42 -0700

    #13 ImageView added in the add event activity and can take photo from camera or select an image from gallery (still can't place the image into the image view)

--- app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/habitEvents/AddHabitEventActivity.java
+++ app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/habitEvents/AddHabitEventActivity.java
@@ -1,19 +1,40 @@
 package com.CMPUT301F21T30.Habiteer.ui.habitEvents;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
-import android.widget.CalendarView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.CMPUT301F21T30.Habiteer.R;
 import com.CMPUT301F21T30.Habiteer.Session;
 import com.CMPUT301F21T30.Habiteer.ui.habit.Habit;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Class to add new habit event
@@ -31,6 +52,12 @@ public class AddHabitEventActivity extends AppCompatActivity {
     String eventComment;
 
     Button addButton;
+    private ImageView selectedImage;
+    public static final int CAM_PERM_CODE = 101;
+    public static final int CAM_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 105;
+
+    String currentPhotoPath;
 
     /**
      * To set habit event layout, get intent and set event date
@@ -65,6 +92,190 @@ public class AddHabitEventActivity extends AppCompatActivity {
             }
         });
 
+        //To add image to
+        selectedImage = findViewById(R.id.event_image);
+
+        selectedImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectOptions();
+            }
+        });
+
+    }
+
+    /**
+     * click on the image to open an alert dialog to display options
+     * alert dialog has options for camera, gallery and cancel
+     */
+    private void selectOptions() {
+        final CharSequence[] items = {"Camera", "Gallery", "Cancel"};
+
+        AlertDialog.Builder builder= new AlertDialog.Builder(AddHabitEventActivity.this);
+        builder.setTitle("Add an Image");
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (items[i].equals("Camera")){
+                    askCameraPermission();
+                }
+                else if (items[i].equals("Gallery")){
+                    openGallery();
+                }
+                else if (items[i].equals("Cancel")){
+                    Toast.makeText(AddHabitEventActivity.this, "Canceled", Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    /**
+     * asks the user for camera permission
+     */
+    private void askCameraPermission() {
+        if(ContextCompat.checkSelfPermission(AddHabitEventActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAM_PERM_CODE);
+        }
+        else {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    /**
+     * checks if permission is granted to the camera or not
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAM_PERM_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
+            }
+            else{
+                Toast.makeText(this, " Camera Permission is required to use camera", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAM_REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                File f = new File(currentPhotoPath);
+                selectedImage.setImageURI(Uri.fromFile(f));
+                Log.d("tag", "Absolute Url of image is" + Uri.fromFile(f));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+
+                //upload image to db
+            }
+
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + "." + getFileExt (contentUri);
+                File f = new File(currentPhotoPath);
+                Log.d("tag 2", "onActivityResult: Gallery Image Uri" + imageFileName);
+                selectedImage.setImageURI(contentUri);
+
+                //upload image to db
+            }
+
+        }
+
+
+    }
+
+    /**
+     *
+     * @param contentUri
+     * @return the extension of the image that is selected from the gallery
+     */
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mine = MimeTypeMap.getSingleton();
+        return mine.getExtensionFromMimeType(c.getType(contentUri));
+    }
+
+    /**
+     * creates an image file name and stores the image in an storage directory
+     * @return image
+     * @throws IOException
+     */
+
+    private File createImageFile() throws IOException {
+        //Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        //File storageDir =  getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+
+        );
+        //Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    /**
+     * opens the camera intent to take picture
+     * stores the Uri of the image
+     */
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+
+        //create the file where the photo should go
+        File photoFile = null;
+        try{
+            photoFile = createImageFile();
+
+        } catch (IOException e) {
+            //Error occurred while creating the file
+            //e.printStackTrace();
+        }
+        //Continue if the file was successfully created
+        if (photoFile != null){
+            Uri photoURI = FileProvider.getUriForFile(AddHabitEventActivity.this, "com.CMPUT301F21T30.android.fileprovider", photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, CAM_REQUEST_CODE);
+        }
+
+    }
+
+    /**
+     * opens gallery
+     */
+    public void openGallery (){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, GALLERY_REQUEST_CODE);
     }
 
     /**
