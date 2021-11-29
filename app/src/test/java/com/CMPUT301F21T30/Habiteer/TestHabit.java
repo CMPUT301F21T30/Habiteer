9de85d8ed5fd7f5c0713553096a02cffb04ff664
Geoff Banh <38046772+geoff-B@users.noreply.github.com>
2021-11-28 17:29:11 -0700

    #63 attempt to fix habit testing; something still wrong with gradle probably

--- app/src/test/java/com/CMPUT301F21T30/Habiteer/TestHabit.java
+++ app/src/test/java/com/CMPUT301F21T30/Habiteer/TestHabit.java
@@ -5,10 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 
 import com.CMPUT301F21T30.Habiteer.ui.habit.Habit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
 import java.util.Collection;
 import java.util.Date;
@@ -18,134 +22,18 @@ import java.util.ListIterator;
 
 import ca.antonious.materialdaypicker.MaterialDayPicker;
 
+//@RunWith(AndroidJUnit4::class)
 public class TestHabit {
+
     private Habit mockHabit() {
 
         Date startDate = new Date();
         Date endDate = new Date();
-        List<MaterialDayPicker.Weekday> weekdayList = new List<MaterialDayPicker.Weekday>() {
-            @Override
-            public int size() {
-                return 0;
-            }
+        List<MaterialDayPicker.Weekday> weekdayList;
+        MaterialDayPicker dayPicker = new MaterialDayPicker(ApplicationProvider.getApplicationContext());
+        dayPicker.selectDay(MaterialDayPicker.Weekday.FRIDAY);
+        weekdayList = dayPicker.getSelectedDays();
 
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(@Nullable Object o) {
-                return false;
-            }
-
-            @NonNull
-            @Override
-            public Iterator<MaterialDayPicker.Weekday> iterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @NonNull
-            @Override
-            public <T> T[] toArray(@NonNull T[] ts) {
-                return null;
-            }
-
-            @Override
-            public boolean add(MaterialDayPicker.Weekday weekday) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(@Nullable Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(@NonNull Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(@NonNull Collection<? extends MaterialDayPicker.Weekday> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int i, @NonNull Collection<? extends MaterialDayPicker.Weekday> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(@NonNull Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(@NonNull Collection<?> collection) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public MaterialDayPicker.Weekday get(int i) {
-                return null;
-            }
-
-            @Override
-            public MaterialDayPicker.Weekday set(int i, MaterialDayPicker.Weekday weekday) {
-                return null;
-            }
-
-            @Override
-            public void add(int i, MaterialDayPicker.Weekday weekday) {
-
-            }
-
-            @Override
-            public MaterialDayPicker.Weekday remove(int i) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(@Nullable Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(@Nullable Object o) {
-                return 0;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<MaterialDayPicker.Weekday> listIterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<MaterialDayPicker.Weekday> listIterator(int i) {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public List<MaterialDayPicker.Weekday> subList(int i, int i1) {
-                return null;
-            }
-        };
-        weekdayList.add(MaterialDayPicker.Weekday.FRIDAY);
 
 
         return new Habit("Reading", startDate, endDate, weekdayList, "new habit" );
@@ -162,7 +50,7 @@ public class TestHabit {
     @Test
     void testGetWeekdayList(){
         Habit habit = mockHabit();
-        assertEquals(0, habit.getWeekdayList().size());
+        assertEquals(1, habit.getWeekdayList().size());
     }
 
     @Test
