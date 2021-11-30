77b6b9aab8173203d40f78c7749037a1b172a822
Geoff Banh <38046772+geoff-B@users.noreply.github.com>
2021-11-30 01:39:27 -0700

    #16 convert notification activity to fragment

--- app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/habit/ListHabitFragment.java
+++ app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/habit/ListHabitFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.ItemTouchHelper;
@@ -112,7 +113,7 @@ public class ListHabitFragment extends Fragment implements TabLayout.OnTabSelect
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.user_profile_menu:
-                startActivity(new Intent(getContext(), UserNotification.class));
+                Navigation.findNavController(requireView()).navigate(R.id.action_navigation_listhabit_to_userNotification);
                 return true;
         }
 
