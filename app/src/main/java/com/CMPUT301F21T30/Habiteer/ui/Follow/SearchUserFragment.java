aaed8534c9167928b6f33cc912099a65a9560c28
Geoff Banh <38046772+geoff-B@users.noreply.github.com>
2021-11-30 10:27:57 -0700

    #12 layout improvements, code cleanup

--- app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/Follow/SearchUserFragment.java
+++ app/src/main/java/com/CMPUT301F21T30/Habiteer/ui/Follow/SearchUserFragment.java
@@ -1,8 +1,15 @@
 package com.CMPUT301F21T30.Habiteer.ui.Follow;
 
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -10,39 +17,19 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.SearchView;
-
 import com.CMPUT301F21T30.Habiteer.R;
 import com.CMPUT301F21T30.Habiteer.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+
 
 /**
  *This fragment shows the search page and allows the user to search for other users by typing
@@ -55,9 +42,7 @@ public class SearchUserFragment extends Fragment {
     private RecyclerView searchRecycler;
     private List<User> searchList;
     private SearchUserAdapter searchUserAdapter;
-    private SearchUserViewModel mViewModel;
-    FirebaseFirestore db;
-    FirebaseAuth firebaseAuth;
+
 
 
     public static SearchUserFragment newInstance() {
@@ -83,10 +68,10 @@ public class SearchUserFragment extends Fragment {
 
 
         searchList = new ArrayList<User>();
-        mViewModel = new ViewModelProvider(this).get(SearchUserViewModel.class);
         searchRecycler = root.findViewById(R.id.searchList);
         recyclerSetup();
         searchView = root.findViewById(R.id.searchView);
+        searchView.setIconifiedByDefault(false);
 
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
